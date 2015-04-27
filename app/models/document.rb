801297c83d920247e093bca2b2307977b7244cc3
Doug Graiser <dmgraiser@gmail.com>
2015-04-26 22:19:43 -0400

    Working on S3 still

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,4 @@
 class Document < ActiveRecord::Base
 	has_attached_file :file
-	 validates_attachment_content_type :file, :content_type => ["image/jpg", "image/gif", "image/png", "application/msword"]
+	 validates_attachment_content_type :file, :content_type => ["image/jpg", "image/gif", "image/png", "application/msword", "application/pdf"]
 end
